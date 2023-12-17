ce3cc35b0ff93e3cd9ffe551eabf9010f33221ce
Xuyanli-Hub <1721456615@qq.com>
2023-12-17 22:39:22 +0800

    15-双变量数据分析2.ipynb

--- output/push(1).sh
+++ output/push(1).sh
@@ -2,5 +2,5 @@ git config --global user.name 'Xuyanli-Hub'
 git config --global user.email '1721456615@qq.com'
 git config --global http.sslverify false
 git add .
-git commit -m 自动推送
+git commit -m $1
 git push
